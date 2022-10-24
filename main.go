a46395f8154761c711fb34f48d0be89801c9340d
VH🔥 <vherman@vf-soft.com>
2022-10-24 20:09:21 +0300

    create a pod or print error

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -23,6 +24,22 @@ func main() {
 	}
 	for _, n := range nodeList.Items {
 		fmt.Println(n.Name)
+	}
+	newPod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-pod",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{Name: "busybox", Image: "busybox:latest", Command: []string{"sleep", "100000"}},
+			},
+		},
+	}
+	pod, err := clientset.CoreV1().Pods("default").Create(context.Background(), newPod, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(pod)
 	}
 
 }
